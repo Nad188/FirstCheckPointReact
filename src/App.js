1d963d053a484a20a7028ce9d25009882eb548ed
Nad188 <nader.benhammouda@gmail.com>
2021-05-19 20:39:27 +0100

    FirstReactCheckPoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<h1>CheckPoint-Form</h1>
+<Form>
+   <div className="lyout">
+  <Form.Group >
+    <Form.Label className="titles">First name</Form.Label>
+    <Form.Control className='space' placeholder="Name"/>
+  </Form.Group>
+  <Form.Group >
+    <Form.Label className="titles">Last name</Form.Label>
+    <Form.Control className='space' placeholder="Last name"/>
+  </Form.Group>
+  </div>
+  <div className="lyout">
+  <Form.Group >
+    <Form.Label className="titles">Address</Form.Label>
+    <Form.Control className='space' placeholder="Address"/>
+  </Form.Group>
+  <Form.Group >
+    <Form.Label className="titles">University</Form.Label>
+    <Form.Control className='space' placeholder="University"/>
+  </Form.Group>
+  </div>
+  <div className="lyout">
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label className="titles">Email address</Form.Label>
+    <Form.Control className='space' type="email" placeholder="Enter email" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label className="titles">Password</Form.Label>
+    <Form.Control className='space' type="password" placeholder="Password" />
+  </Form.Group>
+  </div>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check className='titles' type="checkbox" label="Check me out" />
+  </Form.Group>
+  
+  <Button className='button' variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</div>
+
   );
 }
 
 export default App;
+
+
